f196794e6944b098cbd0c7128f80c0101a0e896c
Ogbondah Jessica <ogbondahj@gmail.com>
2022-10-30 15:21:35 +0100

    first commit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function calcAge(birthYear) {
 	const age = 2037 - birthYear;
 
 	function printAge() {
-		const output = `${firstName},you are ${age}, born in ${birthYear}`;
+		const output = `${firstName}, you are ${age}, born in ${birthYear}`;
 		console.log(output);
 
 		if (birthYear >= 1981 && birthYear <= 1996) {
-			const str = `Oh, and you're a millenial,${firstName}`;
+			const str = `Oh, and you're a millenial, ${firstName}`;
 			console.log(str);
 		}
 	}
