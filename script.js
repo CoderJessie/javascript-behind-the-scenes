bc738c5eb276d878a03c633af592e5a404dc954b
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-03 13:25:23 +0100

    first commit

--- script.js
+++ script.js
@@ -47,11 +47,24 @@ const year = 1991;
 
 //Hoisting with functions
 console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addExpr);
+// console.log(addArrow(2, 3));
+
 function addDecl(a, b) {
 	return a + b;
 }
 
-const addExpr = function (a, b) {
+var addExpr = function (a, b) {
 	return a + b;
 };
 const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+	console.log(`All products deleted!`);
+}
