bbb869556c72d7eae6fd8e1536c18b5a664aa354
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-04 17:00:47 +0100

    first commit

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
-*/
+
 
 //Hoisting with variables
 console.log(me);
@@ -77,3 +77,19 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+//The 'this' keyword in practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+	console.log(2037 - birthYear);
+	console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+	console.log(2037 - birthYear);
+	console.log(this);
+};
+calcAgeArrow(1980);
