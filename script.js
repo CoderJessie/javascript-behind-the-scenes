903b6bfe2ff9f9f6faa61a0d57bfdca179e5af90
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-08 14:52:08 +0100

    first commit

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ matilda.calcAge();
 
 // const f = jonas.calcAge;
 // f(); The 'this' keyword here is undefined so this variable would only display 'undefined' in the console.
-*/
+
 
 // Regular functions vs Arrow functions
 // var firstName = 'Matilda';
@@ -160,3 +160,21 @@ var addArrow = (a, b) => {
 	return a + b;
 };
 addArrow(2, 5, 8); // argument keyword doesnt exist in arrow function
+
+*/
+
+//Primitives Vs Objects
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+	name: 'Jonas',
+	age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
