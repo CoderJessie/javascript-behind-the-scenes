143e9cf118f492a1c72524f7f2f8656963958349
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-09 14:53:32 +0100

    how to copy an object

--- script.js
+++ script.js
@@ -201,3 +201,13 @@ console.log('After marriage:', marriedJessica);
 // marriedJessica = {};
 
 // Copying Objects
+const jessica2 = {
+	firstName: 'Jessica',
+	lastName: 'Williams',
+	age: 27,
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
