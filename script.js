eba251d774690e807191f1f22e0bba4d93ac9ded
Ogbondah Jessica <ogbondahj@gmail.com>
2022-11-10 14:58:04 +0100

    first commit

--- script.js
+++ script.js
@@ -210,8 +210,6 @@ const jessica2 = {
 
 const jessicaCopy = Object.assign({}, jessica2);
 jessicaCopy.lastName = 'Davis';
-console.log('Before marriage:', jessica2);
-console.log('After marriage:', jessicaCopy);
 
 jessicaCopy.family.push('Mary');
 jessicaCopy.family.push('John');
